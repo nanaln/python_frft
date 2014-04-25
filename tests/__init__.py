646d2b2b54d4113b9d5fea94940ccc8dbfedc0d6
Nils Werner <nils.werner@gmail.com>
2014-04-25 09:00:45 +0200

    Unittests, most of them failing

---
+++ tests/__init__.py
@@ -0,0 +1,21 @@
+import frft
+import numpy
+
+
+def precision(a):
+    """
+    Test if transform-inverse identity holds
+
+    """
+    siglen = 2048
+
+    i = numpy.random.random(siglen)
+    x = frft.frft(i, a)
+    y = frft.ifrft(x, a)
+
+    assert numpy.allclose(i, y)
+
+
+def test_precision():
+    for a in xrange(40):
+        yield precision, a / 10.0
